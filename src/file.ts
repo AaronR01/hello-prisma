c4f5d31a108ec42408434e1307702accd4099e26
Aaron <aaron@ice.ufjf.br>
2024-06-10 18:52:01 -0300

    comunicação funcionando

--- src/file.ts
+++ src/file.ts
@@ -1,5 +1,8 @@
 import express, { Request, Response } from 'express';
+import { PrismaClient } from '@prisma/client'
+import { stringify } from 'querystring';
 
+const prisma = new PrismaClient()
 const app = express();
 const port = 3000;
 
@@ -8,13 +11,25 @@ app.get('/', (req: Request, res: Response) => {
 });
 
 app.get("/api/CreateUser/",(req: Request, res: Response) => {
-    res.send("okayC ").status(200)
+    res.send("okay").status(200)
 });
 
 app.get("/api/CreateUser/:nome",(req: Request, res: Response) => {
     const nome = req.body
-    res.send("okayC "+nome).status(200)
+    res.send("okay"+nome).status(200)
 });
+
+app.get('/publish/', async (req, res) => {
+    const post = await prisma.user.create({
+            data: {
+                name: "nome",
+                password: "senha",
+                telefone: "123-4123-51223-333",
+            }
+        })
+        console.log(post)
+        res.send(post).status(200)
+    })
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
